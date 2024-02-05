2442b0ed9cf77ad9254b53a7c5cd457e53561c4c
Jose Flores <jfloresxc@gmail.com>
2024-02-05 17:28:31 -0500

    Add - Portfolio mode facebook

---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+const colors = require("tailwindcss/colors");
+const fontsize = require("tailwindcss/defaultTheme").fontSize;
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      ...colors,
+      primary: "#0866FF", //rgb(20 184 166)
+      secondary: {
+        100: "#65676B",
+        200: "#E4EBE6",
+        300: "#CCD0D5",
+      },
+      dark: "#050505",
+    },
+    fontSize: {
+      regular: "15px",
+      ...fontsize,
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
